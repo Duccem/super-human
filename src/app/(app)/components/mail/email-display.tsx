7343d886c6773cb1a4ee1d4e6f0b4c8d6b2338fa
Jose Veliz <ducen29@gmail.com>
2024-10-13 00:54:14 -0400

    Format the email-display

--- src/app/(app)/components/mail/email-display.tsx
+++ src/app/(app)/components/mail/email-display.tsx
@@ -12,42 +12,50 @@ type Props = {
   email: Primitives<Email>;
 };
 const EmailDisplay = ({ email }: Props) => {
-  const { account } = useThreads()
-    const letterRef = useRef<HTMLDivElement>(null);
+  const { account } = useThreads();
+  const letterRef = useRef<HTMLDivElement>(null);
 
+  React.useEffect(() => {
+    if (letterRef.current) {
+      const gmailQuote = letterRef.current.querySelector('div[class*="_gmail_quote"]');
+      if (gmailQuote) {
+        gmailQuote.innerHTML = '';
+      }
+    }
+  }, [email]);
 
-    React.useEffect(() => {
-        if (letterRef.current) {
-            const gmailQuote = letterRef.current.querySelector('div[class*="_gmail_quote"]');
-            if (gmailQuote) {
-                gmailQuote.innerHTML = '';
-            }
-        }
-    }, [email]);
+  const isMe = account?.emailAddress === email.from?.address;
 
-    const isMe = account?.emailAddress === email.from?.address
-
-    return (
-        <div className={cn('border rounded-md p-4', {
-          'border-l-gray-900 border-l-4': isMe
-      })} ref={letterRef}>
-            <div className="flex items-center justify-between gap-2">
-                <div className='flex items-center gap-2'>
-                    {!isMe && <Avatar name={email.from?.name ?? email.from?.address} email={email.from?.address} size='35' textSizeRatio={2} round={true} />}
-                    <span className='font-medium'>
-                        {isMe ? 'Me' : email.from?.address}
-                    </span>
-                </div>
-                <p className='text-xs text-muted-foreground'>
-                    {formatDistanceToNow(email.sentAt ?? new Date(), {
-                        addSuffix: true,
-                    })}
-                </p>
-            </div>
-            <div className="h-4"></div>
-            <Letter className='bg-white rounded-md text-black' html={email?.body ?? ""} />
+  return (
+    <div
+      className={cn('border rounded-md p-4', {
+        'border-l-gray-900 border-l-4': isMe,
+      })}
+      ref={letterRef}
+    >
+      <div className="flex items-center justify-between gap-2">
+        <div className="flex items-center gap-2">
+          {!isMe && (
+            <Avatar
+              name={email.from?.name ?? email.from?.address}
+              email={email.from?.address}
+              size="35"
+              textSizeRatio={2}
+              round={true}
+            />
+          )}
+          <span className="font-medium">{isMe ? 'Me' : email.from?.address}</span>
         </div>
-    )
+        <p className="text-xs text-muted-foreground">
+          {formatDistanceToNow(email.sentAt ?? new Date(), {
+            addSuffix: true,
+          })}
+        </p>
+      </div>
+      <div className="h-4"></div>
+      <Letter className="bg-white rounded-md text-black" html={email?.body ?? ''} />
+    </div>
+  );
 };
 
 export default EmailDisplay;
